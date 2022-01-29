e59233dfcfe90e4e5879c32c6a6db60490410397
divergnight <47478344+divergnight@users.noreply.github.com>
2022-01-29 17:48:45 +0100

    Add all routes and 404 Page

--- src/Components/AppNavbar/AppNavbar.jsx
+++ src/Components/AppNavbar/AppNavbar.jsx
@@ -2,22 +2,22 @@ import { Navbar, Container, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default function AppNavbar() {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">App BTC-SPA</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Accueil
-            </Nav.Link>
-            <Nav.Link as={Link} to="/page">
-              Gérer
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
+	return (
+		<Navbar bg="light" expand="lg">
+			<Container>
+				<Navbar.Brand href="#home">App BTC-SPA</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link as={Link} to="/">
+							Accueil
+						</Nav.Link>
+						<Nav.Link as={Link} to="/op-members/navy">
+							Gérer
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	)
 }
--- src/index.js
+++ src/index.js
@@ -3,36 +3,37 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
 import AppNavbar from './Components/AppNavbar/AppNavbar'
 import AccueilPage from './Pages/AccueilPage/AccueilPage'
 import Page from './Pages/Page/Page'
 import AddPage from './Pages/AddPage/AddPage'
 import UpdatePage from './Pages/UpdatePage/UpdatePage'
-
+import Redirect from './Pages/Redirect/Redirect'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <main>
-      <Router>
-        <header className="mb-5">
-          <AppNavbar></AppNavbar>
-        </header>
+	<React.StrictMode>
+		<Router>
+			<header>
+				<AppNavbar />
+			</header>
+			<Routes>
+				<Route index element={<AccueilPage />} />
 
-        <Routes>
-          <Route path="/" element={<AccueilPage />} />
-          <Route path="/page" element={<Page />} />
-          <Route path="/page/add" element={<AddPage />} />
-          <Route path="/page/:id" element={<UpdatePage />} />
-        </Routes>
-      </Router>
-    </main>
-  </React.StrictMode>,
-  document.getElementById('root')
+				<Route path="/op-members/:group" element={<Outlet />}>
+					<Route index element={<Page />} />
+					<Route path="add" element={<AddPage />} />
+					<Route path="edit-:index" element={<UpdatePage />} />
+				</Route>
+
+				<Route path="*" element={<Redirect />} />
+			</Routes>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
-
